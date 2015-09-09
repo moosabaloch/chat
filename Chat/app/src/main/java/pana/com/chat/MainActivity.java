6bd07f9571770689a300a2f0d2c61059bb16469b
moosabaloch <moosa.bh@gmail.com>
2015-09-09 13:18:23 +0500

    New Commit Added

--- Chat/app/src/main/java/pana/com/chat/MainActivity.java
+++ Chat/app/src/main/java/pana/com/chat/MainActivity.java
@@ -6,9 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-//myCommmit
-    //Commit by moosa
-    @Override
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
