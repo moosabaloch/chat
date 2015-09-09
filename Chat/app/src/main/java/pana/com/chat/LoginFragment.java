8025a32dcb559c52cb06480b3bd2fea5d4b10845
moosabaloch <moosa.bh@gmail.com>
2015-09-09 13:23:46 +0500

    xml login

--- Chat/app/src/main/java/pana/com/chat/LoginFragment.java
+++ Chat/app/src/main/java/pana/com/chat/LoginFragment.java
@@ -1,7 +1,7 @@
 package pana.com.chat;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +17,7 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
 }
