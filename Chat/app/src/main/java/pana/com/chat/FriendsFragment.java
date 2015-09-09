108b20937fe0372af03693d0809798a9f89882c5
moosabaloch <moosa.bh@gmail.com>
2015-09-09 14:36:03 +0500

    Change in login and friend frag

--- Chat/app/src/main/java/pana/com/chat/FriendsFragment.java
+++ Chat/app/src/main/java/pana/com/chat/FriendsFragment.java
@@ -1,7 +1,7 @@
 package pana.com.chat;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
--- Chat/app/src/main/java/pana/com/chat/LoginFragment.java
+++ Chat/app/src/main/java/pana/com/chat/LoginFragment.java
@@ -2,6 +2,8 @@ package pana.com.chat;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,7 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         loginButton = (Button) view.findViewById(R.id.buttonLoginFragmentLogin);
         createAccountButton = (Button) view.findViewById(R.id.buttonLoginFragmentCreateAccount);
@@ -34,7 +36,13 @@ public class LoginFragment extends Fragment {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                FragmentManager fragmentManager2 = getFragmentManager();
+                FragmentTransaction fragmentTransaction2 = fragmentManager2.beginTransaction();
+                FriendsFragment friendsFragment = new FriendsFragment();
+                fragmentTransaction2.addToBackStack("xyz");
+                fragmentTransaction2.hide(LoginFragment.this);
+                fragmentTransaction2.add(android.R.id.content, friendsFragment);
+                fragmentTransaction2.commit();
             }
         });
     }
