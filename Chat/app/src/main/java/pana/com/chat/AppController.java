e2aa824435c8c7a546912d91a58359577a4fd11f
moosabaloch <moosa.bh@gmail.com>
2015-09-09 13:47:56 +0500

    AppController SingleTon Class Added

---
+++ Chat/app/src/main/java/pana/com/chat/AppController.java
@@ -0,0 +1,71 @@
+package pana.com.chat;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Moosa on 9/9/2015.
+ * Dear Maintainer
+ * When i wrote this code Only i and God knew What it was.
+ * Now only God Knows..!
+ * So if you are done trying to optimize this routine and Failed
+ * Please increment the following counter as the warning to the next Guy.
+ * TOTAL_HOURS_WASTED_HERE=1
+ */
+public class AppController extends Application {
+
+    public static final String TAG = AppController.class
+            .getSimpleName();
+    private static AppController mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader() {
+        getRequestQueue();
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
+        return this.mImageLoader;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
--- Chat/app/src/main/java/pana/com/chat/LoginFragment.java
+++ Chat/app/src/main/java/pana/com/chat/LoginFragment.java
@@ -5,11 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class LoginFragment extends Fragment {
+    private Button loginButton, createAccountButton;
+    private EditText emailToLogin, passwordToLogin;
 
     public LoginFragment() {
     }
@@ -18,6 +22,15 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+        loginButton = (Button) view.findViewById(R.id.buttonLoginFragmentLogin);
+        createAccountButton = (Button) view.findViewById(R.id.buttonLoginFragmentCreateAccount);
+        emailToLogin = (EditText) view.findViewById(R.id.editTextLoginFragmentEmailAddress);
+        passwordToLogin = (EditText) view.findViewById(R.id.editTextLoginFragmentPassword);
+        createNewAccount();
         return view;
+    }
+
+    private void createNewAccount() {
+
     }
 }
