4c20d46c32a9848dabea47674f77d947b003a7a9
moosabaloch <moosa.bh@gmail.com>
2015-09-15 16:25:41 +0500

    Group Chat 1st Phase Completed

--- Chat/app/src/main/java/pana/com/chat/GroupChatFragment.java
+++ Chat/app/src/main/java/pana/com/chat/GroupChatFragment.java
@@ -1,18 +1,30 @@
 package pana.com.chat;
 
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GroupChatFragment extends Fragment {
-
+    private Firebase firebaseURL;
+    private ImageButton sendMessageButton;
+    private EditText sendMessageText;
+    private ListView chatListView;
+    private TextView friendName, friendEmail;
+    private Button logoutButton;
 
     public GroupChatFragment() {
         // Required empty public constructor
@@ -23,7 +35,15 @@ public class GroupChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_group_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        sendMessageButton = (ImageButton) view.findViewById(R.id.chatFragmentButtonSendMessage);
+        sendMessageText = (EditText) view.findViewById(R.id.chatFragmentEditTextWriteMessageHere);
+        friendName = (TextView) view.findViewById(R.id.chatFragmentTextViewFriendName);
+        friendEmail = (TextView) view.findViewById(R.id.chatFragmentTextViewFriendEmailView);
+        logoutButton = (Button) view.findViewById(R.id.chatFragmentButtonLogout);
+        chatListView = (ListView) view.findViewById(R.id.chatFragmentListViewChatMessages);
+
         return view;
     }
 
