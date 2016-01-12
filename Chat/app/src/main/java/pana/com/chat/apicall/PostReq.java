6061f714d6f7d577196dba451851e9904d42bdc0
moosabaloch <moosa.bh@gmail.com>
2016-01-12 13:29:42 +0500

    Added Branch

--- Chat/app/src/main/java/pana/com/chat/apicall/PostReq.java
+++ Chat/app/src/main/java/pana/com/chat/apicall/PostReq.java
@@ -16,7 +16,7 @@ import pana.com.chat.Util.Utils;
  * Created by Moosa on 1/1/2016.
  * Dear Maintainer
  * When i wrote this code Only i and God knew What it was.
- * Now only God Knows..!
+ * Now only God Knows....!
  * So if you are done trying to optimize this routine and Failed
  * Please increment the following counter as the warning to the next Guy.
  * TOTAL_HOURS_WASTED_HERE=1
